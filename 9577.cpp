e118da1d206ce8713b9e2dc75fd95c9fad6de67b
Maelys P <165888009+Maelys-p@users.noreply.github.com>
2024-04-10 14:20:11 +0300

    Eolymp 9577
    Stole most of the code off some weird guy I met on an overpass in *the city*. 
    He only asked for half a pack of cigs in return.

---
+++ 9577.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <bitset>
+ 
+#define ull unsigned long long
+ 
+
+using namespace std;
+
+ull finder(ull n){
+ 
+    
+    bitset<64> bits(n);
+    bool flag = false;
+    string pieces = bits.to_string();
+    pieces.erase(0, pieces.find('1'));
+    int length = pieces.size();
+    int halflength = length / 2 + length % 2;
+    stack<int> OO;
+   
+    
+    for (int i = 0; i < halflength; ++i)
+    {
+        int pair = length - i - 1;
+        if (pieces[i] == '0')
+        {
+          OO.push(i);
+          if (pieces[pair] == '1') flag = true;
+        }
+        else if (pieces[pair] == '0') flag = false;
+    }
+
+    if (OO.empty()) 
+    {
+        for (int i = length / 2; i < length; ++i) {
+            pieces[i] = '1';
+        }
+    }
+    
+    else if (flag) 
+    {
+        int lastIndex = OO.top();
+        pieces[lastIndex] = '1';
+        pieces[length - lastIndex - 1] = '1';
+
+        for (int i = 1; i < lastIndex; ++i) 
+            pieces[length - i - 1] = pieces[i];
+         
+        for (int i = lastIndex + 1; i < halflength; ++i)
+        { 
+            pieces[i] = '0';
+            pieces[length - i - 1] = '0';
+        }
+    } 
+    else
+    {
+        for (int i = 0; i < halflength; ++i) 
+            pieces[length - i - 1] = pieces[i];         
+    }
+    
+    return bitset<64>(pieces).to_ullong();
+}
+
+
+
+int main() {
+    ull n;
+    cin >> n;
+    cout << finder(n);
+}
