29cf6d6cf85ba9895968f80d30c9c02ae9a94eb1
Maelys P <165888009+Maelys-p@users.noreply.github.com>
2024-04-13 19:11:35 +0300

    Eolymp 16
    The solution is simple, the question is simple, but that dubious necromancer zaza that the author was hotboxing his room with, while phrasing the question was pure fucking evil

---
+++ 16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    int S,K,A,B;
+    std::cin >> S >> K >> A >> B;
+    bool possible = false;
+    int N;
+    for(int i = 1; i < 1001; ++i)
+    {
+        int l = A-K*i;
+        int m = (B - l*S);
+        if(l<=0 || m<0) continue;
+        N = m / i;
+        possible = true;
+        break;
+
+    }
+    if(possible) std::cout<<N;
+    else std::cout<<-1;
+    return 0;
+}
